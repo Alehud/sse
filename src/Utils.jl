06c9ef9b60bfafa5b5b3d15734bc3a5dfcbfa86e
alehud <alehud1995@gmail.com>
2022-07-25 19:57:45 -0400

    Added base2int function and offset_zero flag.

--- src/Utils.jl
+++ src/Utils.jl
@@ -1,6 +1,6 @@
-export int2base
+export int2base, base2int
 
-function int2base(n::Integer, b::Integer, num_digits::Integer)
+function int2base(n::Integer, b::Integer, num_digits::Integer; offset_zero::Bool=false)
     if (n < 0)
         raise(error("`n` has to be non-negative."))
     elseif b ≤ 0
@@ -17,5 +17,21 @@ function int2base(n::Integer, b::Integer, num_digits::Integer)
     if n ≠ 0
         raise(error("The number ", n ," does not fit in ", num_digits, " digits in base ", b, "."))
     end
+    if offset_zero
+        digits .+= 1
+    end
     return digits[end:-1:1]
+end
+
+
+function base2int(digits::Vector{<:Integer}, b::Integer; offset_zero::Bool=false)
+    digits_temp = copy(digits)
+    if offset_zero
+        digits_temp .-= 1
+    end
+    n = 0
+    for (i, d) in enumerate(digits_temp[end:-1:1])
+        n += d * b^(i-1)
+    end
+    return n
 end
